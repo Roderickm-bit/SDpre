15ee8fc2f96991baa49c0f09175a80c4b55785c0
RoderickM05 <roderickm.536@gmail.com>
2023-05-02 13:56:42 -0400

    complete objective1lab2

---
+++ Labs/Objective1Lab2.java
@@ -0,0 +1,10 @@
+public class Objective1Lab2 {
+	public static void main(String[] args) {
+		System.Out.Println( "----------------")
+		System.Out.Println( "| Grocery List |")
+		System.Out.Println( "| 1. Milk		|")
+		System.Out.Println( "| 2. Eggs		|")
+		System.Out.Println( "| 3. Bread     |")
+		System.Out.Println( "----------------");
+	}
+}
