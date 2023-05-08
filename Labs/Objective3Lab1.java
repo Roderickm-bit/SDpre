56cb7a49248237c28402da0fdb13198f713d8957
RoderickM05 <roderickm.536@gmail.com>
2023-05-08 14:20:53 -0400

    add Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,10 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1 = 12; 
+    double num2 = 6;
+    double num3 = 3;
+    int average = 12 + 6 + 3;
+    average /= 3; 
+    System.out.println(average);
+  }
+}
