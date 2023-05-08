b0cc247cb1bc8fddeb6def7d6c55dac76aa784b8
RoderickM05 <roderickm.536@gmail.com>
2023-05-08 14:34:41 -0400

    add Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,8 @@
+public class Objective3Lab2 {
+	public static void main(String[] args) {
+		double side1 = Math.sqrt(3 * 3);
+		double side2 = Math.sqrt(4 * 4);
+		double hypotenuse = Math.sqrt(9) + Math.sqrt(16);
+		System.out.println("The hypotenuse of a triangle with sides 9.0 and 16.0 is 7.0");
+	}
+}
