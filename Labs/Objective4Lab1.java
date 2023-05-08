ce026ef4eef671225b4f7c2685da398c47ba5d1f
RoderickM05 <roderickm.536@gmail.com>
2023-05-08 14:07:12 -0400

    add Objective4Lab1

---
+++ Labs/Objective4Lab1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Objective4Lab1 {
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("What is your first name.");
+    String fname = scanner.nextLine();
+    
+    System.out.println("What is your last name.");
+    String lname = scanner.nextLine();
+    
+    System.out.println("What is your favoriteAnimal?");
+    String favoriteAnimal = scanner.nextLine();
+   
+    System.out.println("What is your favoriteFood?");
+    String favoriteFood = scanner.nextLine();
+    
+    System.out.println("What is your favoriteSong?");
+    String favoriteSong = scanner.nextLine();
+
+ 
+//TODO prompt the user to get input for all of the String variables.
+
+    //TODO print the output formatted to look like the expected output using String concatenation.
+
+  }
+}
