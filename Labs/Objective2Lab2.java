e823b9d5f2a545b4ce167c79e8470fce9428e38b
RoderickM05 <roderickm.536@gmail.com>
2023-05-08 14:13:13 -0400

    add Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Charles Peterson";
+		System.out.println(name);
+		int age = 47;
+		System.out.println(age);
+		String state = "Arkansas";
+		System.out.println(state);
+	}
+}
