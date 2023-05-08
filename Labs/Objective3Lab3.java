818cf0984f86cae588d95260fa68ac2411298bb1
RoderickM05 <roderickm.536@gmail.com>
2023-05-08 14:45:53 -0400

    add Objective3Lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,10 @@
+public class Objective3Lab3 {
+	public static void main(String[] args) {
+		int first = 1 + (int)(Math.random() * 6);
+		System.out.println(first);
+		int second = 1 + (int)(Math.random() * 6);
+		System.out.println(second);
+		int third = 1 + (int)(Math.random() * 6);
+		System.out.println(third);
+	}
+}
