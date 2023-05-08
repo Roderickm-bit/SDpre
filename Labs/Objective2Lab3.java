f816cc56653f764a19b4286e4e5e88df6f32f66a
RoderickM05 <roderickm.536@gmail.com>
2023-05-08 14:15:16 -0400

    add Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+	public static void main(String[] args) {
+		int year = 1994;
+		System.out.println(year);
+		String month = "March";
+		System.out.println(month);
+		String weekend = "false";
+		System.out.println(weekend);
+
+	}
+}
