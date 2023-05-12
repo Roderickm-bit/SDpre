d9c837239ed08512d617409f10d915ce34186c3a
RoderickM05 <roderickm.536@gmail.com>
2023-05-11 23:50:11 -0400

    add Objective8Lab3.java

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,21 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+    int counter = 1;
+
+    for(int w=1; w<=20; w++){
+      System.out.println(counter);
+      counter = w + 1;
+    if(counter % 2 == 1) {
+      System.out.println(" is even ");
+    }
+    else {
+      System.out.println(" is odd ");
+    }
+    
+    }
+    }
+
+    
+    }
+    
+
