602b5a57fe8f64f28ebcea2f7125ea3d243dd73f
RoderickM05 <roderickm.536@gmail.com>
2023-05-11 22:45:45 -0400

    Objective7Lab4.java

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,15 @@
+public class Objective7Lab4 {
+	public static void main(String[] args) {
+		int count = 1;
+		int sum = 20;
+		
+
+		while(count <= 20) {
+			System.out.println(sum);
+			sum = count + sum;
+			count = count + 1;
+		}
+			
+
+				} 
+	}
