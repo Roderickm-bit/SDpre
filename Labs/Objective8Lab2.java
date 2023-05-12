a3b77650614cb09f7e7062cf624455dd93c06278
RoderickM05 <roderickm.536@gmail.com>
2023-05-11 22:59:28 -0400

    add Objective8Lab2.java

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,13 @@
+public class Objective8Lab2 {
+	public static void main(String[] args) {
+		int sum = 20;
+		
+
+		for(int i=1; i<=20; i++) {
+			System.out.println(sum);
+			sum = i + sum;
+		}
+			
+
+				} 
+	}
